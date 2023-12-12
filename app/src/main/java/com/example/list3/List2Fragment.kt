40a809e4820569e5926fee01b09497e9a4fabfbd
Marcin27141 <marcin.tkocz@op.pl>
2023-12-12 14:46:53 +0100

    added list adapter to recycler view

--- app/src/main/java/com/example/list3/List2Fragment.kt
+++ app/src/main/java/com/example/list3/List2Fragment.kt
@@ -11,9 +11,11 @@ import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.list3.Database.DBItem
 import com.example.list3.Database.MyRepository
@@ -38,6 +40,7 @@ class List2Fragment : Fragment() {
     private var param2: String? = null
     private lateinit var dataRepo: MyRepository
     private lateinit var binding: FragmentList2Binding
+    private lateinit var myAdapter: MyListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,19 +65,13 @@ class List2Fragment : Fragment() {
 
         val recyclerView: RecyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        val adapter = MyAdapter(dataRepo.getAnimals()!!)
+
+        myAdapter = MyListAdapter(onItemAction)
+        //val adapter = MyAdapter(dataRepo.getAnimals()!!)
 
         val floatingButton: FloatingActionButton = binding.FAB
         floatingButton.setOnClickListener {
             findNavController().navigate(R.id.action_recyclerViewList_to_addAnimalFragment)
-        }
-
-        parentFragmentManager.setFragmentResultListener("item_added", viewLifecycleOwner) {
-                _, _ -> adapter.refreshList()
-        }
-
-        parentFragmentManager.setFragmentResultListener("item_modified", viewLifecycleOwner) {
-                _, _ -> adapter.refreshList()
         }
 
         val dividerItemDecoration = DividerItemDecoration(requireActivity(), RecyclerView.VERTICAL)
@@ -82,8 +79,93 @@ class List2Fragment : Fragment() {
             ?.let { drawable -> dividerItemDecoration.setDrawable(drawable) }
         binding.recyclerView.addItemDecoration(dividerItemDecoration)
 
-        recyclerView.adapter = adapter
 
+        recyclerView.adapter = myAdapter
+        myAdapter.submitList(dataRepo.getAnimals())
+
+    }
+
+    companion object {
+        private val DiffCallback = object: DiffUtil.ItemCallback<DBItem>() {
+            override fun areItemsTheSame(oldItem: DBItem, newItem: DBItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+            override fun areContentsTheSame(oldItem: DBItem, newItem: DBItem): Boolean {
+                return oldItem.name == newItem.name &&
+                oldItem.latinName == newItem.latinName &&
+                oldItem.animalType == newItem.animalType &&
+                oldItem.strength == newItem.strength &&
+                oldItem.health == newItem.health &&
+                oldItem.isDeadly == newItem.isDeadly;
+            }
+        }
+    }
+
+    var onItemAction : (animal:DBItem, action:Int) -> Unit = { item, action ->
+        when (action) {
+            1 -> Toast.makeText(requireContext(), "You clicked: " + item.name, Toast.LENGTH_SHORT).show()
+            2 -> {
+                dataRepo.deleteAnimal(item)
+                Toast.makeText(requireContext(), "Deleted: " + item.name, Toast.LENGTH_SHORT).show()
+                myAdapter.submitList(dataRepo.getAnimals())
+            }
+        }
+    }
+
+    inner class MyListAdapter(private val onItemAction: (item: DBItem, action:Int)-> Unit) :
+        ListAdapter<DBItem, MyListAdapter.MyViewHolder>(DiffCallback) {
+        inner class MyViewHolder(viewBinding : AnimalItemBinding) :
+            RecyclerView.ViewHolder(viewBinding.root) {
+            val title: TextView = viewBinding.animalName
+            val subtitle: TextView = viewBinding.latinName
+            val icon: ImageView = viewBinding.itemIcon
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val viewBinding = AnimalItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return MyViewHolder(viewBinding)
+        }
+
+        private fun sunItemIcon(holder: MyViewHolder, animal: DBItem) {
+            val iconSrc = when (animal.animalType) {
+                DBItem.AnimalType.BIRD -> R.drawable.bird_icon
+                DBItem.AnimalType.PREDATOR -> R.drawable.predator_icon
+                DBItem.AnimalType.INSECT -> R.drawable.insect_icon
+                DBItem.AnimalType.RODENT -> R.drawable.rodent_icon
+            }
+            holder.icon.setImageResource(iconSrc)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            val currItem = getItem(position)
+
+            holder.title.text = currItem.name
+            holder.subtitle.text = currItem.latinName
+            sunItemIcon(holder, currItem)
+            holder.title.setTextColor(if (currItem.isDeadly) resources.getColor(R.color.red) else resources.getColor(R.color.green))
+
+            holder.itemView.setOnClickListener {
+                val position = holder.adapterPosition
+                onItemAction(getItem(position), 1)
+            }
+
+            holder.itemView.setOnLongClickListener {
+                val position = holder.adapterPosition
+                val item = getItem(position)
+                val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
+                builder
+                    .setTitle("Are you sure?")
+                    .setMessage("You are going to delete " + item.name + ". Do you want to continue?")
+                    .setPositiveButton("Yes") { dialog, _ ->
+                        onItemAction(item, 2)
+                    }
+                    .setNegativeButton("No") { dialog, _ ->
+                        dialog.cancel()
+                    }
+                builder.create().show()
+                true
+            }
+        }
     }
 
     inner class MyAdapter(var data: MutableList<DBItem>) :
@@ -135,17 +217,17 @@ class List2Fragment : Fragment() {
             holder.itemView.setOnLongClickListener {
                 val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
                 builder
-                    .setTitle("Are you sure?")
-                    .setMessage("You are going to delete " + data[position].name + ". Do you want to continue?")
-                    .setPositiveButton("Yes") { dialog, _ ->
-                        if (dataRepo.deleteAnimal(data[position])) {
-                            data = dataRepo.getAnimals()!!
-                            notifyDataSetChanged()
-                        }
-                    }
-                    .setNegativeButton("No") { dialog, _ ->
-                        dialog.cancel()
-                    }
+        .setTitle("Are you sure?")
+        .setMessage("You are going to delete " + data[position].name + ". Do you want to continue?")
+        .setPositiveButton("Yes") { dialog, _ ->
+            if (dataRepo.deleteAnimal(data[position])) {
+                data = dataRepo.getAnimals()!!
+                notifyDataSetChanged()
+            }
+        }
+        .setNegativeButton("No") { dialog, _ ->
+            dialog.cancel()
+        }
                 builder.create().show()
                 true
             }
