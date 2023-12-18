4def51df0998b5b67acb8a18d38157a34f10d2d4
Marcin27141 <marcin.tkocz@op.pl>
2023-12-18 15:31:18 +0100

    setting invitation image from storage

--- app/src/main/java/com/example/list3/SetImageFragment.kt
+++ app/src/main/java/com/example/list3/SetImageFragment.kt
@@ -1,12 +1,15 @@
 package com.example.list3
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.list3.databinding.FragmentSetImageBinding
 import com.example.list3.databinding.FragmentSwipeBinding
@@ -19,47 +22,78 @@ class SetImageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSetImageBinding.inflate(inflater, container, false);
-
-        val viewPagerAdapter = ImageAdapter(requireActivity())
         val viewPager = binding.viewPager
-        viewPager.adapter = viewPagerAdapter
 
-        val initialIcon = PreferencesManager.getInstance().getHomeIcon(requireActivity())
-        viewPager.setCurrentItem(viewPagerAdapter.getIconTabNumber(initialIcon), false)
+        val dataRepo = ImageRepo.getInstance(requireContext())
+        val imagesList = dataRepo.getFilesList()
+        val adapter = imagesList?.let {
+            StoredImageAdapter(requireActivity(), it)
+        }
+        if (adapter == null) {
+            Toast.makeText(requireContext(), "Invalid data", Toast.LENGTH_SHORT).show()
+            requireActivity().onBackPressed()
+        }
+        viewPager.adapter = adapter
+
+
+
+
+//        val viewPagerAdapter = ImageAdapter(requireActivity())
+
+//        viewPager.adapter = viewPagerAdapter
+//
+//        val initialIcon = PreferencesManager.getInstance().getHomeIcon(requireActivity())
+//        viewPager.setCurrentItem(viewPagerAdapter.getIconTabNumber(initialIcon), false)
+//
 
         val setButton = binding.setButton
         setButton.setOnClickListener {
             val currentTab = viewPager.currentItem
-            val iconSrc = viewPagerAdapter.getCurrentIconSrc(currentTab)
-            PreferencesManager.getInstance().setHomeIcon(iconSrc, requireActivity())
+            val imageUri = adapter?.getCurrentImageUri(currentTab)
+            PreferencesManager.getInstance().setHomeImage(imageUri!!, requireActivity())
             findNavController().navigate(R.id.mainScreen)
         }
 
         return binding.root
     }
 
-    class ImageAdapter(fa: FragmentActivity): FragmentStateAdapter(fa) {
-        val PAGE_COUNT: Int = 4
-        val PAGE_ICONS: IntArray = intArrayOf(
-            R.drawable.bird_icon,
-            R.drawable.predator_icon,
-            R.drawable.rodent_icon,
-            R.drawable.insect_icon)
+    class StoredImageAdapter(fa: FragmentActivity, val imageList: MutableList<FileItem>): FragmentStateAdapter(fa) {
         override fun getItemCount(): Int {
-            return PAGE_COUNT
+            return imageList.size
         }
 
-        fun getCurrentIconSrc(currentTab: Int) : Int {
-            return PAGE_ICONS[currentTab]
-        }
-
-        fun getIconTabNumber(iconSrc: Int): Int {
-            return PAGE_ICONS.indexOf(iconSrc)
+        fun getCurrentImageUri(currentTab: Int) : Uri {
+            return imageList[currentTab].contentUri!!
         }
 
         override fun createFragment(position: Int): Fragment {
-            return ShowIconFragment.newInstance(PAGE_ICONS[position])
+            return ShowIconFragment.newInstance(imageList[position])
         }
 
     }
+
+//    class ImageAdapter(fa: FragmentActivity): FragmentStateAdapter(fa) {
+//        val PAGE_COUNT: Int = 4
+//        val PAGE_ICONS: IntArray = intArrayOf(
+//            R.drawable.bird_icon,
+//            R.drawable.predator_icon,
+//            R.drawable.rodent_icon,
+//            R.drawable.insect_icon)
+//        override fun getItemCount(): Int {
+//            return PAGE_COUNT
+//        }
+//
+//        fun getCurrentIconSrc(currentTab: Int) : Int {
+//            return PAGE_ICONS[currentTab]
+//        }
+//
+//        fun getIconTabNumber(iconSrc: Int): Int {
+//            return PAGE_ICONS.indexOf(iconSrc)
+//        }
+//
+//        override fun createFragment(position: Int): Fragment {
+//            return ShowIconFragment.newInstance(PAGE_ICONS[position])
+//        }
+//
+//    }
 }
--- app/src/main/java/com/example/list3/SwipeFragment.kt
+++ app/src/main/java/com/example/list3/SwipeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.list3
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.list3.databinding.FragmentSwipeBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
